614c4f38161a9a93eeaecd302b53c861de7177b9
Thiago Oliveira <idd_toliveira@uolinc.com>
2022-02-21 23:15:59 -0300

    Primeiro commit do projeto

---
+++ src/test/java/com/br/estudos/EstudosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.estudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
